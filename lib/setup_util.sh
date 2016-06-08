6913d155f8f197a84e75aa801fbd832540513bfe
Warnar Boekkooi <warnar@boekkooi.net>
2016-06-08 12:45:22 +0200

    Added archive download server support

--- lib/setup_util.sh
+++ lib/setup_util.sh
@@ -1,22 +1,76 @@
 #!/usr/bin/env bash
 
-function php_lfs_download_and_extact() {
+function php_download_and_unpack() {
     echo "Preparing PHP:";
 
-    # Resolve some information
-    local LFS_FILE="${OPENSHIFT_PHP_DIR}/usr/php-${PHP_VERSION}.tar.gz"
-    local DOWNLOAD_FILE="${OPENSHIFT_PHP_DIR}/usr/php-${PHP_VERSION}_actual.tar.gz"
     local EXTRACT_DIR="${OPENSHIFT_PHP_DIR}/usr/"
+    local FILENAME="php-${PHP_VERSION}.tar.gz"
 
-    # Check mimetype (LFS maybe installed)
-    local LFS_FILE_MIMETYPE=$(file -b --mime-type "${LFS_FILE}")
-    if [ "${LFS_FILE_MIMETYPE}" = "application/gzip" ]; then
-        unpack "${LFS_FILE}" "${EXTRACT_DIR}"
-        return 0
+    local TARGET_FILE="${OPENSHIFT_PHP_DIR}/usr/${FILENAME}"
+    local TARGET_FILE_MIMETYPE=$(file -b --mime-type "${TARGET_FILE}")
+
+    local LFS_FILE="${OPENSHIFT_PHP_DIR}/usr/${FILENAME}.lfs"
+
+    # In case the archive exist just unpack
+    if [ "${TARGET_FILE_MIMETYPE}" = "application/gzip" ]; then
+        unpack "${TARGET_FILE}" "${EXTRACT_DIR}"
+        return $?
     fi
-    if [ "${LFS_FILE_MIMETYPE}" != "text/plain" ]; then
-        echo "Unsupported mimetype found for file ${LFS_FILE}"
+
+    # Move the file if it is a LFS file
+    if [ "${TARGET_FILE_MIMETYPE}" = "text/plain" ]; then
+        mv "${TARGET_FILE}" "${LFS_FILE}"
     fi
+
+    # First check if any of the download servers has the file
+    for ARCHIVE_SERVER in "${ARCHIVE_SERVERS[@]}"; do
+        local rc=0
+        php_archive_download "${ARCHIVE_SERVER}/${FILENAME}" "${TARGET_FILE}" || rc=$?
+        if [ $rc -eq 0 ]; then
+            unpack "${TARGET_FILE}" "${EXTRACT_DIR}"
+            return $?
+        fi
+    done
+
+    # Fallback to use github LFS
+    php_lfs_download "${LFS_FILE}" "${TARGET_FILE}"
+    unpack "${TARGET_FILE}" "${EXTRACT_DIR}"
+    return $?
+}
+
+function php_archive_download() {
+    if [ -z "${1}" ] || [ -z "${2}" ]; then
+        echo "php_archive_download: expected two arguments"
+        exit -1
+    fi
+
+    # Resolve some information
+    local DOWNLOAD_URL="${1}"
+    local DOWNLOAD_FILE="${2}"
+
+    # Download the file
+    echo "- Downloading package from ${DOWNLOAD_URL}"
+
+    local rc=0
+    wget --progress=dot --output-document="${DOWNLOAD_FILE}" "${DOWNLOAD_URL}" || rc=$?
+    if [ ${rc} -ne 0 ]; then
+        rm -f  "${DOWNLOAD_FILE}"
+        echo "Failed to download ${DOWNLOAD_FILE} from ${DOWNLOAD_URL}"
+        return 1
+    fi
+
+    return 0
+}
+
+function php_lfs_download() {
+    if [ -z "${1}" ] || [ -z "${2}" ]; then
+        echo "php_lfs_download: expected two arguments"
+        exit -1
+    fi
+
+    # Resolve some information
+    local LFS_FILE="${1}"
+    local DOWNLOAD_FILE="${2}"
 
     # Fetch the LFS OID
     echo "- Fetching package OID"
@@ -28,52 +82,55 @@ function php_lfs_download_and_extact() {
     local LFS_OID="${BASH_REMATCH[2]}"
     local LFS_METADATA_URL="${LFS_ENDPOINT}/objects/${LFS_OID}"
 
-    # Download file if needed
-    if [ ! -f "${DOWNLOAD_FILE}" ]; then
-        # Fetch download link
-        echo "- Fetching download URL"
-        local LFS_JSON=$(
-          wget \
-            --progress=dot \
-            --output-document=- \
-            --header='Accept: application/vnd.git-lfs+json' \
-            --header='Content-Type: application/vnd.git-lfs+json' \
-            "${LFS_METADATA_URL}"
-        );
-        if [ $? -ne 0 ]; then
-            echo "Failed to download ${LFS_FILE} metadata from ${LFS_METADATA_URL}"
-            exit 1
-        fi
-
-        local DOWNLOAD_URL=$(echo "${LFS_JSON}" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["_links"]["download"]["href"]')
-        if [ $? -ne 0 ]; then
-            echo "Failed to parse metadata from ${LFS_METADATA_URL}"
-            exit 1
-        fi
-
-        # Download the file
-        echo "- Downloading package"
-        wget --progress=dot --output-document="${DOWNLOAD_FILE}" "${DOWNLOAD_URL}"
-        if [ $? -ne 0 ]; then
-            rm -f  "${DOWNLOAD_FILE}"
-            echo "Failed to download ${LFS_FILE} from ${DOWNLOAD_URL}"
-        fi
-    else
-        echo "- Skipping download package exists"
+    # Fetch download link
+    echo "- Fetching download URL"
+    local LFS_JSON=$(
+      wget \
+        --progress=dot \
+        --output-document=- \
+        --header='Accept: application/vnd.git-lfs+json' \
+        --header='Content-Type: application/vnd.git-lfs+json' \
+        "${LFS_METADATA_URL}"
+    );
+    if [ $? -ne 0 ]; then
+        echo "Failed to download ${LFS_FILE} metadata from ${LFS_METADATA_URL}"
+        exit 1
     fi
 
-    unpack "${DOWNLOAD_FILE}" "${EXTRACT_DIR}"
+    local DOWNLOAD_URL=$(echo "${LFS_JSON}" | python -c 'import json,sys;obj=json.load(sys.stdin);print obj["_links"]["download"]["href"]')
+    if [ $? -ne 0 ]; then
+        echo "Failed to parse metadata from ${LFS_METADATA_URL}"
+        exit 1
+    fi
+
+    # Download the file
+    echo "- Downloading package"
+    wget --progress=dot --output-document="${DOWNLOAD_FILE}" "${DOWNLOAD_URL}"
+    if [ $? -ne 0 ]; then
+        rm -f  "${DOWNLOAD_FILE}"
+        echo "Failed to download ${LFS_FILE} from ${DOWNLOAD_URL}"
+        exit 1
+    fi
 
     return 0
 }
 
 function unpack() {
     if [ -z "${1}" ] || [ -z "${2}" ]; then
-        echo "extract: expected two arguments"
+        echo "unpack: expected two arguments"
         exit -1
     fi
 
     set +e
+
+    echo "- Checking MD5"
+    pushd "$( dirname "${1}" )"
+    md5sum --check "${1}.md5"
+    if [ $? -ne 0 ]; then
+        echo "Invalid file signature!"
+        exit 1
+    fi
+    popd
 
     echo "- Extracting"
     tar xfz "${1}" --directory="${2}"
